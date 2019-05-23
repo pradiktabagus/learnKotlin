c5b5ca5787756661a1e59359f8622bee035a0035
diktabagus <pradiktabagus@gmail.com>
2019-05-23 17:48:37 +0700

    variable

--- src/variable.kt
+++ src/variable.kt
@@ -1,8 +1,29 @@
 fun main(args: Array<String>){
 
+    /*
+        variable var dan val,
+        var -> dapat di isi kembali nilainya
+        val -> tidak dapat di isi kembali nilai konstan
+
+        Tipe data :
+        String = "nama negara"
+        Int    = 32
+        Double  = 22.4
+     */
+//================================  contoh variable dengan var  ================
+
     var name = "Daebak"
     var lastName = "Dariyu"
-
     println("$name dan $lastName")
-    println(lastName)
+
+//=========================== contoh variable dengan val ===================
+
+    val pi:Double = 3.14  // tipe data double
+    val umur:Int = 24   // tipe data integer
+
+    println("nilai dari bilangan pi adalah $pi dan saya berumur $umur")
+
+//=================================
+
+
 }
